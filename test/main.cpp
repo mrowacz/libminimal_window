35ce8e82e59455e71e362753a848ba9acf56f755
Łukasz Czerwiński <mrowacz@gmail.com>
2018-03-25 00:32:35 +0100

    test: add mouse support
    Signed-off-by: Łukasz Czerwiński <mrowacz@gmail.com>

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,29 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
 #include <SDL.h>
+
+using namespace std;
+volatile bool end_flag;
+
+void mouse_task()
+{
+    std::cout << "mouse task started" << std::endl;
+    while(!end_flag) {
+        int x, y;
+        SDL_PumpEvents();
+        SDL_GetMouseState(&x, &y);
+        std::cout << "mouse [" << x << "," << y << "]" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    std::cout << "mouse task ended" << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv;
-    
+
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window *window = SDL_CreateWindow(
@@ -20,7 +39,11 @@ int main(int argc, char *argv[])
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(3000);
+    auto mouse_thread = std::thread(mouse_task);
+    SDL_Delay(10000);
+    end_flag = true;
+    std::cout << "Ending TestApp" << std::endl;
+    mouse_thread.join();
 
     SDL_DestroyWindow(window);
     SDL_Quit();
